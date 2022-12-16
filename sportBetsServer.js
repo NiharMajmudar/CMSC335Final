3cd256faf54d7243ee774de2b24e7aff858d12ce
NiharMajmudar <niharmajmudar@comcast.net>
2022-12-15 19:30:00 -0500

    Use api and get mongo auth working

--- sportBetsServer.js
+++ sportBetsServer.js
@@ -5,12 +5,13 @@ const express = require("express");
 const bodyParser = require("body-parser"); 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { response } = require('express');
+const axios = require('axios')
 const app = express();
 
-require("dotenv").config({ path: path.resolve(__dirname, '.env') }) 
+require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') });
 app.use(express.static(__dirname));
 
-const portNumber = process.env.portNumber || 5000;
+const portNumber = process.env.portNumber || 5001;
 
 process.stdin.setEncoding("utf8"); /* encoding */
 
@@ -20,12 +21,15 @@ const dbName = process.env.MONGO_DB_NAME;
 const collection = process.env.MONGO_COLLECTION;
 const databaseAndCollection = {db: dbName, collection:collection};
 
-const uri = `mongodb+srv://${userName}:${password}@cluster0.1hptff1.mongodb.net/?retryWrites=true&w=majority`;
+console.log(userName);
+console.log(password);
+
+const uri = `mongodb+srv://${userName}:${password}@cluster0.njiausk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.listen(portNumber);
 
-console.log(`Server is listening on the port ${portNumber}`);
+console.log(`Click here to access server: http://localhost:${portNumber}`);
 process.stdout.write("Type stop to shutdown the server: ");
 
 process.stdin.on('readable', () => {  /* on equivalent to addEventListener */
@@ -71,10 +75,6 @@ app.post("/processBet", async (request, response) => {
     }
 });
 
-app.get("/todaysBets", (request, response) => {
-    response.render("todaysBets");
-});
-
 app.get("/betHistory", (request, response) => {
     response.render("betHistory");
 });
@@ -84,3 +84,51 @@ async function insertBet(client, databaseAndCollection, bet) {
 
     console.log(`Bet entry created with id ${result.insertedId}`);
 }
+/* Nihar's Part */
+app.get("/todaysBets", async (request, response) => {
+    const apiKey = '16bfdfc27321c2c5c873016bd22cf0ba'
+    const daysFrom = 2;
+    // const sports_keys = [{'key': 'americanfootball_nfl', 'title': 'NFL'},
+    //  {'key': 'basketball_nba', 'title': 'NBA'}, 
+    //  {'key': 'basketball_ncaab', 'title': 'NCAAB'}];
+    // let sportKey = sports_keys[0]['key'];
+    let scores = await axios.get(`https://api.the-odds-api.com/v4/sports/basketball_nba/scores`, {
+        params: {
+            apiKey,
+            daysFrom,
+        }
+    })
+    .then(response => {
+        return response.data;
+    
+    })
+    .catch(error => {
+        console.log('There was an error with your request');
+    })
+    let table = "<table border=\"1\"><tr><th>Home</th><th>Points</th><th>Away</th><th>Points</th></tr>";
+    scores.forEach(score => {
+        curr_score = score['scores'];
+        if (curr_score == null) {
+            return;
+        }
+        let array_scores = [];
+        let curr = "";
+        curr_score.forEach(team => {
+            array_scores.push(team['name']);
+            array_scores.push(team['score']);
+        });
+        curr += "<tr><td>";
+        curr += array_scores[0];
+        curr += "</td><td>";
+        curr += array_scores[1];
+        curr += "</td><td>";
+        curr += array_scores[2];
+        curr += "</td><td>";
+        curr += array_scores[3];
+        curr += "</td></tr>";
+        table += curr;
+    });
+    table += "</table>"
+    variables = {scoreTable: table};
+    response.render("todaysBets", variables);
+});
